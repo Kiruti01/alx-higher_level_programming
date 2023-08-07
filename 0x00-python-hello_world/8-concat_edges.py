588a485bcd182f68895459402065ff09e53f38a6
Kiruti01 <mikekirutic@gmail.com>
2023-08-07 13:59:26 +0300

    edit task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+
+
+substr1 = str[39:67]
+substr2 = str[114:119]
+substr3 = str[:6]
+
+result = substr1 + substr2 + substr3
+print(result)
