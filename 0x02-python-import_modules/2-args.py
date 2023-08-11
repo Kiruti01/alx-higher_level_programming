a37c92d3dc3dbffb4f88b4ffc0998d854aa7bcd9
Kiruti01 <mikekirutic@gmail.com>
2023-08-11 19:07:35 +0300

    pycode fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print the nmber of and list of arguments"""
+    """print the number of and list of arguments"""
     import sys
 
-    count = len(sys.argv) -1
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments")
     elif count == 1:
