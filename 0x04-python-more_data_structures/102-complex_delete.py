21dac4a50832327154edf88441268511e85064c0
Kiruti01 <mikekirutic@gmail.com>
2023-08-16 14:20:54 +0300

    task 15 fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     keys_2del = []
     for value in a_dictionary:
-        if a_dictionary[key] == value:
+        if a_dictionary[value] == value:
             keys_2del.append(value)
     for value in keys_2del:
         del a_dictionary[value]
