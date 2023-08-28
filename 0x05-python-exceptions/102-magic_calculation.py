91d1cd747a7fcd373f2e989a1e4655a362a52d18
Kiruti01 <mikekirutic@gmail.com>
2023-08-28 15:42:21 +0300

    pycode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,9 +6,10 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += a ** b / i
+
+            result += a ** b / i
         except:
             result = b + a
             break
+
     return (result)
