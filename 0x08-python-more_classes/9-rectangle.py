a53eee42b0fa379eb9980bf2aa97c4dfeb949bc0
Kiruti01 <mikekirutic@gmail.com>
2023-09-04 21:24:01 +0300

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): The symbol used for string representation.
+        print_symbol (any): The symbol for string representation.
     """
 
     number_of_instances = 0
@@ -17,8 +17,8 @@ class Rectangle:
         """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width (int).
+            height (int).
         """
         type(self).number_of_instances += 1
         self.width = width
