6ea5b699545e34d569f7c38d852c9777c8ad3fc4
Mike Kiruti <Kiruti01>
2023-09-12 14:39:00 +0300

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,7 +3,14 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Insert text after line with given string in file
 
+    Args:
+        filename (str): name of file
+        search_string (str): string to search within file
+        new_string (str): string to insert
+    return: appended file
+    """
     text = ""
     with open(filename) as r:
         for line in r:
