d28ec5f8e3e8aa13609218fbee21511602f98a5f
Kiruti01 <mikekirutic@gmail.com>
2023-12-01 14:26:27 +0300

    10en3

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,10 +10,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
 
-    try:
-        r = requests.get("https://api.github.com/user", auth=auth)
-        r.raise_for_status()
-        print(r.json().get("id"))
-    except r.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+   r = requests.get("https://api.github.com/user", auth=auth)
+   print(r.json().get("id"))
