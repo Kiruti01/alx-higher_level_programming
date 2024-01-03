a56f332a9f540a0c05871f9115fbfc2e880848ed
Kiruti01 <mikekirutic@gmail.com>
2024-01-03 13:04:09 +0300

    edit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
+	if (!error) {
+		const todos = JSON.parse(body);
+		let completed = {};
+
+		todos.forEach((todo) => {
+			if (todo.completed) {
+				if (completed[todo.userId] === undefined) {
+					completed[todo.userId] = 1;
+				} else {
+					completed[todo.userId]++;
+				}
+			}
+		});
+
+		console.log(completed);
+	}
 });
